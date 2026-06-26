f2782b8b619a8e4c361ba266c9732ffb1e879256
Rishi Vemireddy <vemireddyrishi@gmail.com>
2026-06-26 01:25:34 -0500

    Splitted data to training and testing sets

--- network.py
+++ network.py
@@ -3,3 +3,10 @@ import numpy as np
 
 data = pd.read_csv('synthetic_diabetes_dataset.csv')
 
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+
+from sklearn.model_selection import train_test_split
+
+X_train, y_train, X_test, y_test = train_test_split(X,y, test_size=0.2, random_state=40)
+
